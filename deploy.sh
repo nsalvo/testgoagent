2d1501b86508f3788eb939c93bdce4459fbec713
Nicola Salvo <nicola.salvo@cloudreach.co.uk>
2015-09-04 18:08:18 +0200

    Changed deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-    
-    if [ -z "${KEYFILE}" ]; then
-	  echo "keys not provided"
-	  exit 1
-    else
-	  ssh -i "${KEYFILE}" docker@$TARGET_HOST < start-container.sh
-     fi
+ssh -i "${KEYFILE}" docker@$TARGET_HOST < start-container.sh
